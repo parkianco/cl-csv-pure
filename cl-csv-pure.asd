95ea41fc13b7632ec96829d9fe046d9ea6737e87
Park Ian Co <opensource@parkian.co>
2026-03-15 00:02:29 -0400

    Relicense from BSD-3-Clause to Apache-2.0

--- cl-csv-pure.asd
+++ cl-csv-pure.asd
@@ -7,7 +7,7 @@
 (asdf:defsystem #:cl-csv-pure
   :description "RFC 4180 compliant CSV parsing for Common Lisp"
   :author "Parkian Company LLC"
-  :license "BSD-3-Clause"
+  :license "Apache-2.0"
   :version "0.1.0"
   :serial t
   :components ((:file "package")
--- test/test-csv-pure.lisp
+++ test/test-csv-pure.lisp
@@ -1,10 +1,10 @@
 ;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
-;; SPDX-License-Identifier: BSD-3-Clause
+;; SPDX-License-Identifier: Apache-2.0
 
 ;;;; test-csv-pure.lisp - Unit tests for csv-pure
 ;;;;
 ;;;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
-;;;; SPDX-License-Identifier: BSD-3-Clause
+;;;; SPDX-License-Identifier: Apache-2.0
 
 (defpackage #:cl-csv-pure.test
   (:use #:cl)
